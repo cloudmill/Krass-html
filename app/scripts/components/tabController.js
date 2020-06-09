b172f0a0def60a7894e43155585852262483d586
Дмитрий Антонов <dma@cloudmill.ru>
2020-06-09 15:59:59 +0300

    [UPDATE] Главную апнул

--- app/scripts/components/tabController.js
+++ app/scripts/components/tabController.js
@@ -4,13 +4,27 @@ import { gsap } from "gsap";
 export default class TabController {
   constructor() {
     this.togleClickerInit();
+    this.tabsMenuInit();
   }
   togleClickerInit() {
-    console.log(gsap)
     $(document).on("click", "[data-toggle]", function () {
       $("#" + $(this).attr("data-toggle")).toggleClass("active");
       // var logo = document.getElementById($(this).attr("data-toggle"));
       // gsap.to(logo, 1, {opacity:"1"});
+    });
+  }
+  tabsMenuInit() {
+    $(document).on("click", "[data-trigger]:not(.active)", function () {
+      $("[data-trigger]").parent().removeClass("active");
+      $("[data-trigger]").parent().removeClass("prev");
+      var index = $(this).parent().index();
+      $("[data-trigger]").each(function(){
+        var i = $(this).parent().index()
+        if(i < index) $(this).parent().addClass('prev')
+      })
+      $(this).parent().addClass("active");
+      $("[data-target]").removeClass("active");
+      $("[data-target='" + $(this).attr("data-trigger") + "']").addClass("active");
     });
   }
 }
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,12 @@
 import $ from "jquery";
 import Swiper from "swiper";
-import TabController from "./components/tabController.js";
+import 'select2';
 
+import TabController from "./components/tabController.js";
 var App = function() {
+  
   var tabs = new TabController();
+  $('.field-select select').select2()
 };
 
 export default App;
