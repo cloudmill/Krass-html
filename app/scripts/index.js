59e7d3b79663e69c9935347c0d070aafe3b75258
szatolokin <staszatolokin28@gmail.com>
2020-10-19 17:45:53 +0300

    [UPDATE] Правки Калькулятор герметика и пены

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -15,55 +15,41 @@ $(document).ready(function () {
 });
 
 function expenseCalculator() {
-  // режим расчета
-  const MODE_1 = "sealant"; // для герметика
-  const MODE_2 = "foam"; // для пены
-  
-  const calculationMode = MODE_1; // режим расчета для герметика по-умолчанию
-  
-  const form = $(".prodInfo-calc"); // форма расчета
+  const form = $(".prodInfo-calc");
 
-  const submitButton = form.find(".field-submit"); // кнопка для расчета
+  const submitButton = form.find(".field-submit");
 
-  // поля с данными
   const xField = form.find("input[name=\"x\"]");
   const yField = form.find("input[name=\"y\"]");
   const zField = form.find("input[name=\"z\"]");
 
-  // добавляем ограничение на ввод для полей
-  inputControl(xField);
-  inputControl(yField);
-  inputControl(zField);
+  inputControl(xField, form.data("x-max"));
+  inputControl(yField, form.data("y-max"));
+  inputControl(zField, form.data("z-max"));
 
-  // нажатие на кнопку для расчета
   submitButton.click(function (event) {
-    event.preventDefault(); // отмета отправки формы
-  
-    // получение значений
+    event.preventDefault();
+
     const xValue = xField.val();
     const yValue = yField.val();
     const zValue = zField.val();
 
-    // валидация значений
     if (isValidInput(xValue, yValue, zValue)) {
       const resultFields = $(".prodInfo-calc-result-item");
       
       const germField = $("#germ");
       const cartField = $("#cart");
 
-      // расчет расхода в литрах
       const germResult = (xValue * 10) * (yValue * 0.01) * (zValue * 0.01);
 
-      // расчет расхода в картриджах
-      const cartValue = 0.3; // объем одного картриджа в литрах
-      const cartResult = Math.ceil(germResult / cartValue);
+      const cartValue = form.data("cart-value");
 
-      // отображение результата
-      const germResultRound = Math.ceil(germResult * 1000) / 1000; // округление объема-результата до 3 цифр после запятой 
+      const cartResult = Math.ceil(germResult / cartValue);
+      const germResultRound = Math.round(germResult * 100) / 100;
+
       germField.text(germResultRound);
       cartField.text(cartResult);
 
-      // первое появление панели с результатами
       if (!resultFields.hasClass("open")) {
         resultFields.addClass("open");
       }
@@ -72,8 +58,7 @@ function expenseCalculator() {
     }
   });
 
-  // ограничение на ввод (только цифры, не начинать с нуля)
-  function inputControl(inputField) {
+  function inputControl(inputField, maxValue) {
     const inputFieldElement = inputField[0];
 
     inputFieldElement.oninput = function (event) {
@@ -87,6 +72,12 @@ function expenseCalculator() {
       } else {
         $(this).val(lastInputStr.substr(0, lastInputStr.length - 1));
       }
+
+      const resultInputValue = $(this).val();
+      if (resultInputValue > maxValue) {
+        // $(this).val(lastInputStr.substr(0, lastInputStr.length - 1));
+        $(this).val(maxValue);
+      }
     };
 
     inputFieldElement.onblur = function (event) {
@@ -98,7 +89,6 @@ function expenseCalculator() {
     };
   }
 
-  // строка - цифра?
   function isDigit(char) {
     if (
       char === "0" ||
@@ -118,9 +108,13 @@ function expenseCalculator() {
     return false;
   }
 
-  // валидация ввода - целое положительное число
   function isValidInput(x, y, z) {
-    if (x > 0 && y > 0 && z > 0) {
+    if (
+      x > 0 && y > 0 && z > 0 &&
+      x <= form.data("x-max") &&
+      y <= form.data("y-max") &&
+      z <= form.data("z-max")
+    ) {
       return true;
     }
 
